797e76bf56e06521f3b9c5d3224a8d0ad3ec31cb
jon <jmcausey77@gmail.com>
2026-09-22 19:07:15 -0500

    clean up

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'cd localservices'
-cd localservices
-echo 'pip install -r requirments'
-pip install -r requirements.txt
-echo 'flask --app flaskr init-db'
-flask --app flaskr init-db
-echo 'Successfully install localservices'
